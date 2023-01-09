38dca2f6978639469128547db9761d5821e76f59
Carol Rieping <carol.rieping@gmail.com>
2023-01-08 18:15:38 -0700

    Add item2 exists and attributes tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -14,7 +14,18 @@ describe Item do
       expect(item1.name).to eq('Chalkware Piggy Bank')
     end
 
+    it 'exists' do
+      item2 = Item.new('Bamboo Picture Frame')
+      
+      expect(item2).to be_a(Item)
+    end
 
-    
+    it 'has attributes' do
+      item2 = Item.new('Bamboo Picture Frame')
+
+      expect(item2.name).to eq('Bamboo Picture Frame')
+    end
+
+
   end
 end
