3d7de696628ef8522b819077503b8c93908b1fcd
Carol Rieping <carol.rieping@gmail.com>
2023-01-08 18:11:20 -0700

    Add item1 attributes test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,8 +4,17 @@ describe Item do
   describe 'initialize' do
     it 'exists' do
       item1 = Item.new('Chalkware Piggy Bank')
-
+      
       expect(item1).to be_a(Item)
     end
+
+    it 'has attributes' do
+      item1 = Item.new('Chalkware Piggy Bank')
+
+      expect(item1.name).to eq('Chalkware Piggy Bank')
+    end
+
+
+    
   end
 end
