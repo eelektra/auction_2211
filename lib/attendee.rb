a3f8065c4eb05042ee415cd6e436248baeba6332
Carol Rieping <carol.rieping@gmail.com>
2023-01-08 18:47:49 -0700

    Add attendee attributes test

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -4,8 +4,8 @@ class Attendee
        :budget
 
   def initialize(attributes)
-    @name = attributes
-    @budget = attributes
+    @name = attributes[:name]
+    @budget = attributes[:budget]
   end
 
 end
--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -9,6 +9,13 @@ describe Attendee do
       expect(attendee).to be_a(Attendee)
     end
 
+    it 'has attributes' do
+      attendee = Attendee.new(name: 'Megan', budget: '$50')
+
+      expect(attendee.name).to eq("Megan")
+      expect(attendee.budget).to eq(50)
+    end
+
     
   end
 end
